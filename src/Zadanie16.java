718c972daa468aa4b69ad92bcb1396162ba44688
DenisGerasimenko <61081562+DenisGerasimenko@users.noreply.github.com>
2020-02-25 20:28:13 +0300

    commit 1

---
+++ src/Zadanie16.java
@@ -0,0 +1,21 @@
+public class Zadanie16 {
+    public static void main(String[] args) {
+        int mas[] = new int[9];
+        int i;
+        int k;
+        for (i = 0; i < mas.length; i++) {
+            mas[i] = (int) (Math.random() * 100);
+            System.out.print(mas[i] + " ");
+        }
+        System.out.println();
+        for (i = 0; i <= mas.length / 2; i++) {
+            k = mas[i];
+            mas[i] = mas[mas.length - i - 1];
+            mas[mas.length - 1 - i] = k;
+        }
+        for (i = 0; i < mas.length; i++) {
+            System.out.print(mas[i] + " ");
+
+        }
+    }
+}
