0ef0d67da00a134097ff5cc127e62ad100710d06
DenisGerasimenko <61081562+DenisGerasimenko@users.noreply.github.com>
2020-02-27 15:19:58 +0300

    commit 1

---
+++ src/Zadanie18.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+/**
+ * Поиск знаков препинания
+ */
+public class Zadanie18 {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("[!.,;:?\\-]");
+        Scanner scanner = new Scanner(System.in);
+        Matcher matcher = pattern.matcher(scanner.nextLine());
+        int count = 0;
+        while (matcher.find()){
+            count++;
+        }
+        System.out.println(count);
+    }
+}
