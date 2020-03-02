5bb299e3d6a4c9ef99c24412d63b465c1321c92e
DenisGerasimenko <61081562+DenisGerasimenko@users.noreply.github.com>
2020-03-02 15:23:55 +0300

    commit 1

---
+++ src/Zadanie20.java
@@ -0,0 +1,18 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+
+public class Zadanie20{
+
+    public static void main(String[] args) {
+        String s = "Java — объектно-ориентированный язык "
+                + "программирования, разработанный компанией "
+                + "Sun Microsystems (в последующем приобретённой "
+                + "компанией Oracle).";
+        Pattern pattern = Pattern.compile("([a-zа-я])\\b");
+        Matcher matcher = pattern.matcher(s);
+        while (matcher.find()) {
+            System.out.print(matcher.group(1));
+        }
+    }
+}
