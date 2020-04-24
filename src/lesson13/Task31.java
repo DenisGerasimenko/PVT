93d692d7abec5286302cc3193df8098952b1aadd
DenisGerasimenko <61081562+DenisGerasimenko@users.noreply.github.com>
2020-04-24 23:03:04 +0300

    commit 1

--- src/lesson13/Task31.java
+++ src/lesson13/Task31.java
@@ -1,15 +1,15 @@
 package lesson13;
 
+import java.io.FileInputStream;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.NoSuchElementException;
 import java.util.Scanner;
 
 public class Task31 {
-    public static void main (String[] args) throws java.lang.Exception
-    {
-        Scanner in = new Scanner(System.in);
-        ArrayList <Integer> numbers = new ArrayList <Integer>();
+    public static void main(String[] args) throws java.lang.Exception {
+        Scanner in = new Scanner(new FileInputStream("D:\\file.txt"));
+        ArrayList<Integer> numbers = new ArrayList<Integer>();
 
         while (true) {
 
@@ -18,11 +18,9 @@ public class Task31 {
                 String currentToken = in.next();
                 numbers.add(Integer.parseInt(currentToken));
 
-            }
-            catch (NoSuchElementException e) {
+            } catch (NoSuchElementException e) {
                 break;
-            }
-            catch (NumberFormatException e) {
+            } catch (NumberFormatException e) {
                 continue;
             }
 
@@ -30,7 +28,7 @@ public class Task31 {
 
         int sum = 0;
         System.out.println("Числа в тексте:");
-        for (Integer element: numbers) {
+        for (Integer element : numbers) {
             System.out.print(element + " ");
             sum += element;
         }
@@ -38,19 +36,19 @@ public class Task31 {
         System.out.println();
         System.out.println("Сумма элементов: " + sum);
 
-        ArrayList <Integer> numbersNoRepeat = new ArrayList<Integer>();
-        HashSet <Integer> alreadyMet = new HashSet<Integer>();
+        ArrayList<Integer> numbersNoRepeat = new ArrayList<Integer>();
+        HashSet<Integer> alreadyMet = new HashSet<Integer>();
 
-        for (Integer element: numbers) {
+        for (Integer element : numbers) {
             if (!alreadyMet.contains(element)) {
                 numbersNoRepeat.add(element);
                 alreadyMet.add(element);
             }
         }
 
-        
+
         System.out.println("Числа в тексте без повторений:");
-        for (Integer element: numbersNoRepeat) {
+        for (Integer element : numbersNoRepeat) {
             System.out.print(element + " ");
             sum += element;
         }
--- src/lessson14/Task32.java
+++ src/lessson14/Task32.java
@@ -1,4 +1,39 @@
 package lessson14;
 
+
+import java.io.*;
+
 public class Task32 {
+    public static void main(String[] args) {
+        try (DataOutputStream os = new DataOutputStream(new FileOutputStream("D:\\file.txt"))) {
+            for (int i = 0; i < 20; i++) {
+                os.writeInt((int) (Math.random() * 20));
+            }
+            System.out.println("Файл записан");
+
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        double res = 0;
+        try (DataInputStream is = new DataInputStream(new FileInputStream("D:\\file.txt"))) {
+            for (int i = 0; i < 20; i++) {
+                int x;
+                x = is.readInt();
+                res += x;
+                System.out.print(x + " ");
+
+            }
+        } catch (IOException ex) {
+            System.out.println(ex.getMessage());
+        }
+        System.out.println(" ");
+        res /= 20;
+        System.out.println("среднее арифметическое равняется: " + res);
+
+    }
 }
+
+
+
+
+
